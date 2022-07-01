d8c814b6d07fbd0dafe6664f29a7fd9f7edade01
Sergey <duxxless53@ya.ru>
2022-07-01 05:42:05 +0300

    Test.

--- UserInterface/ViewController.swift
+++ UserInterface/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var redImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemRed
+        return imageView
+    }()
+    
+    var yellowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemYellow
+        return imageView
+    }()
+    
+    var greenImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemGreen
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray5
+        title = "ViewController"
+        
+        view.addSubview(redImageView)
+        view.addSubview(greenImageView)
+        view.addSubview(yellowImageView)
+        
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.systemBlue.cgColor
+        layer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        redImageView.layer.addSublayer(layer)
+        
+        redImageView.frame = CGRect(x: 100, y: 200, width: 200, height: 200)
+        
     }
-
-
 }
 
